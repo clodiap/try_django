60022293c32a4f53d84e32650482c19ae689396d
clodiap <clodiapp@gmail.com>
2018-08-12 19:30:56 +0200

    django raw html forms

--- products/views.py
+++ products/views.py
@@ -5,15 +5,24 @@ from .forms import ProductForm
 # Create your views here.
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-
-    context = {
-        'form': form
-    }
+    if request.method == "POST":
+        my_new_title = request.POST.get('title')
+        print(my_new_title)
+        # Product.objects.create(title=my_new_title)
+    context = {}
     return render(request, "products/product_create.html", context)
+
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
