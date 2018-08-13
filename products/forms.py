25c2645d05a7a4a655b5e4e3c9c8e1aa44fdb264
clodiap <clodiapp@gmail.com>
2018-08-13 15:48:15 +0200

    forms validation

--- products/forms.py
+++ products/forms.py
@@ -2,6 +2,20 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title       = forms.CharField(label='',widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description = forms.CharField(
+                        required=False,
+                        widget=forms.Textarea(
+                            attrs={
+                                "class": "new-class-name two",
+                                "placeholder": "Your description",
+                                "id": "new_id_textarea",
+                                "rows":5,
+                                "cols": 50
+                            }
+                        )
+                    )
+    price       = forms.DecimalField(initial=2.99)
     class Meta:
         model = Product
         fields = {
@@ -9,6 +23,13 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         }
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if not "CFE" in title:
+            raise forms.ValidationError("This is not a valid title")
+        return title
+
+
 
 class RawProductForm(forms.Form):
     title       = forms.CharField(label='',widget=forms.TextInput(attrs={"placeholder": "Your title"}))
