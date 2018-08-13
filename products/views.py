40d6e6bf895fc1d8e91ff7d122389a63a5dde5f2
clodiap <clodiapp@gmail.com>
2018-08-13 16:02:12 +0200

    dynamic url routing

--- products/views.py
+++ products/views.py
@@ -4,6 +4,13 @@ from .forms import ProductForm, RawProductForm
 
 # Create your views here.
 
+def dynamic_lookup_view(request, id):
+    obj = Product.objects.get(id=id)
+    context = {
+        "object":obj
+    }
+    return render(request, "products/product_detail.html", context)
+
 # def product_create_view(request):
 #     my_form = RawProductForm()
 #     if request.method == "POST" :
