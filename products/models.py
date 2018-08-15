727eeeacc5b4f37841d5608705bbd4bd5cbc1622
clodiap <clodiapp@gmail.com>
2018-08-15 19:54:48 +0200

    dynamic linking of urls

--- products/models.py
+++ products/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-  title       = models.CharField(max_length=120)
-  description = models.TextField(blank=True, null=True)
-  price       = models.DecimalField(decimal_places=2, max_digits=10000)
-  summary     = models.TextField(blank=False, null=False) #blank=False --> required
-  featured    = models.BooleanField(default=False)
+    title       = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price       = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary     = models.TextField(blank=False, null=False) #blank=False --> required
+    featured    = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return f"/products/{self.id}/"
